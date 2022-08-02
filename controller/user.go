1e3a3c19c1f0a5b1c4006690f3df77595fdaaafd
zhenghao.0321 <zhenghao.0321@bytedance.com>
2022-08-02 20:53:05 +0800

    初始化项目

---
+++ controller/user.go
@@ -0,0 +1,191 @@
+package controller
+
+import (
+	"encoding/json"
+	"file-store/lib"
+	"file-store/model"
+	"file-store/util"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type PrivateInfo struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    string `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	OpenId       string `json:"openid"`
+}
+
+//登录成功获取的QQ用户信息
+type QUserInfo struct {
+	Nickname    string
+	FigureUrlQQ string `json:"figureurl_qq"`
+}
+
+//登录页
+func Login(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
+
+//处理登录
+func HandlerLogin(c *gin.Context) {
+	//conf := lib.LoadServerConfig()
+	//state := "aaaaa"
+	//url := "https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=" + conf.AppId + "&redirect_uri=" + conf.RedirectURI + "&state=" + state
+	//
+	//c.Redirect(http.StatusMovedPermanently, url)
+	//info := model.GetUserInfo("12345")
+	name := c.Request.FormValue("name")
+	value := c.Request.FormValue("pwd")
+	fmt.Println("-------"+name, value)
+	user := &QUserInfo{
+		Nickname:    "zh",
+		FigureUrlQQ: "http:",
+	}
+	marshal, _ := json.Marshal(user)
+	LoginSucceed(string(marshal), "12345", c)
+	//c.Redirect(http.StatusMovedPermanently, "/cloud/index")
+}
+func LoginByPwd(c *gin.Context)  {
+	userName := c.Request.FormValue("name")
+	value := c.Request.FormValue("pwd")
+	fmt.Println("-------"+userName, value)
+	userInfo := model.GetUserInfoByUserName(userName)
+	if len(userInfo.UserName) ==0 {
+		fmt.Println("用户不存在")
+		c.JSON(http.StatusFound,gin.H{"data":"用户不存在,请注册"})
+		return
+	}
+	user := &QUserInfo{
+		Nickname:    userInfo.UserName,
+		FigureUrlQQ: userInfo.ImagePath,
+	}
+	marshal, _ := json.Marshal(user)
+	LoginSucceed(string(marshal), userInfo.OpenId, c)
+
+}
+
+func Register(c *gin.Context)  {
+	userName := c.Request.FormValue("name")
+	pwd := c.Request.FormValue("pwd")
+
+	// 生成openID
+	openID, _ := uuid.NewUUID()
+	log.Println("userName:",userName,";pwd:",pwd,"openID:",openID)
+	model.CreateUser(openID.String(),userName,pwd,"")
+
+}
+
+
+//获取access_token
+func GetQQToken(c *gin.Context) {
+	conf := lib.LoadServerConfig()
+	code := c.Query("code")
+
+	loginUrl := "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=" + conf.AppId + "&client_secret=" + conf.AppKey + "&redirect_uri=" + conf.RedirectURI + "&code=" + code
+
+	response, err := http.Get(loginUrl)
+	if err != nil {
+		fmt.Println("请求错误", err.Error())
+		return
+	}
+	defer response.Body.Close()
+
+	bs, _ := ioutil.ReadAll(response.Body)
+	body := string(bs)
+	resultMap := util.ConvertToMap(body)
+
+	info := &PrivateInfo{}
+	info.AccessToken = resultMap["access_token"]
+	info.RefreshToken = resultMap["refresh_token"]
+	info.ExpiresIn = resultMap["expires_in"]
+
+	GetOpenId(info, c)
+}
+
+//获取QQ openId
+func GetOpenId(info *PrivateInfo, c *gin.Context) {
+	resp, err := http.Get(fmt.Sprintf("%s?access_token=%s", "https://graph.qq.com/oauth2.0/me", info.AccessToken))
+	if err != nil {
+		fmt.Println("GetOpenId Err", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	bs, _ := ioutil.ReadAll(resp.Body)
+	body := string(bs)
+	info.OpenId = body[45:77]
+
+	GetUserInfo(info, c)
+}
+
+//获取QQ用户信息
+func GetUserInfo(info *PrivateInfo, c *gin.Context) {
+	conf := lib.LoadServerConfig()
+	params := url.Values{}
+	params.Add("access_token", info.AccessToken)
+	params.Add("openid", info.OpenId)
+	params.Add("oauth_consumer_key", conf.AppId)
+
+	uri := fmt.Sprintf("https://graph.qq.com/user/get_user_info?%s", params.Encode())
+	resp, err := http.Get(uri)
+	if err != nil {
+		fmt.Println("GetUserInfo Err:", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	bs, _ := ioutil.ReadAll(resp.Body)
+
+	LoginSucceed(string(bs), info.OpenId, c)
+}
+
+//登录成功 处理登录
+func LoginSucceed(userInfo, openId string, c *gin.Context) {
+	var qUserInfo QUserInfo
+	//将数据转为结构体
+	if err := json.Unmarshal([]byte(userInfo), &qUserInfo); err != nil {
+		fmt.Println("转换json失败", err.Error())
+		return
+	}
+
+	//创建一个token
+	hashToken := util.EncodeMd5("token" + string(time.Now().Unix()) + openId)
+	//存入redis
+	if err := lib.SetKey(hashToken, openId, 24*3600); err != nil {
+		fmt.Println("Redis Set Err:", err.Error())
+		return
+	}
+	//设置cookie
+	c.SetCookie("Token", hashToken, 3600*24, "/", "", false, true)
+
+	if ok := model.QueryUserExists(openId); ok { //用户存在直接登录
+		//登录成功重定向到首页
+		c.Redirect(http.StatusMovedPermanently, "/cloud/index")
+	} else {
+		model.CreateUser(openId, qUserInfo.Nickname,"", qUserInfo.FigureUrlQQ)
+		//登录成功重定向到首页
+		c.Redirect(http.StatusMovedPermanently, "/cloud/index")
+	}
+}
+
+//退出登录
+func Logout(c *gin.Context) {
+	token, err := c.Cookie("Token")
+	if err != nil {
+		fmt.Println("cookie", err.Error())
+	}
+
+	if err := lib.DelKey(token); err != nil {
+		fmt.Println("Del Redis Err:", err.Error())
+	}
+
+	c.SetCookie("Token", "", 0, "/", "", false, false)
+	c.Redirect(http.StatusFound, "/")
+}
---
+++ middleware/middleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"file-store/controller"
+	"file-store/lib"
+	"file-store/model"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//检查是否登录中间件
+func CheckLogin(c *gin.Context)  {
+	token, err := c.Cookie("Token")
+	if err != nil {
+		fmt.Println("cookie", err.Error())
+		c.Redirect(http.StatusFound, "/")
+		c.Abort()
+	}
+	if len(token) ==0 {
+		controller.HandlerLogin(c)
+	}
+
+	openId, err := lib.GetKey(token)
+	if err != nil {
+		fmt.Println("Get Redis Err:", err.Error())
+		c.Redirect(http.StatusFound, "/")
+		c.Abort()
+	}
+
+	user := model.GetUserInfo(openId)
+
+	if user.Id == 0 {
+		//校验失败 返回登录页面
+		c.Redirect(http.StatusFound, "/")
+	} else {
+		//校验成功 继续执行
+		c.Set("openId", openId)
+		c.Next()
+	}
+}
